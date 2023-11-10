ef840bdc1d649a63c9f0c7693fb24db5d45018f2
Amil Ali <amil13985@gmail.com>
2023-11-11 00:31:57 +0530

    add

--- audio.js
+++ audio.js
@@ -1,8 +1,9 @@
 var sound = new Howl({
     src: ['https://res.cloudinary.com/djcmgdn0x/video/upload/v1699642560/Ramp_eb8sfs.mp3','https://res.cloudinary.com/djcmgdn0x/video/upload/v1699615026/%EF%BC%B3%EF%BC%B0%EF%BC%A1%EF%BC%A3%EF%BC%A5%EF%BC%B4%EF%BC%B2%EF%BC%A9%EF%BC%B0%EF%BC%A9%EF%BC%A9_Chillwave_-_Synthwave_-_Retrowave_Mix_yhw5th.mp3'],
     autoplay: true,
-    loop: true,
+    loop: true
   });
+  sound.fade(1);
   sound.play();
 
 //   document.addEventListener('click', () => {
