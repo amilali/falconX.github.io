9f6119efc1b4fc05e97f8910f2a6b425b223f48b
Amil Ali <amil13985@gmail.com>
2023-11-13 17:49:26 +0530

    Add images to Projects section and update image effect in script.js

--- script.js
+++ script.js
@@ -57,14 +57,14 @@ Shery.mouseFollower();
       endTrigger: ".last",
       scrub: 1,
     },
-    y: "-300%",
+    y: "-200%",
     ease: Power1,
   });
 
   let sections = document.querySelectorAll(".flefth");
   Shery.imageEffect(".fimg", {
-    style: 4,
-    config: { onMouse: { value: 1 } },
+    style: 2,
+    config:  {"uColor":{"value":false},"uSpeed":{"value":0.6,"range":[0.1,1],"rangep":[1,10]},"uAmplitude":{"value":1.5,"range":[0,5]},"uFrequency":{"value":3.5,"range":[0,10]},"geoVertex":{"range":[1,64],"value":32},"zindex":{"value":"1","range":[-9999999,9999999]},"aspect":{"value":0.851063829787234},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":1.75},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
     slideStyle: (setScroll) => {
       sections.forEach(function (section, index) {
         ScrollTrigger.create({
